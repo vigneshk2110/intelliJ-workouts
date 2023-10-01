4709faa6fd5d2af9b1f3b7ea798292c6bcd386c9
Vignesh <vigneshk2110@gmail.com>
2023-10-01 15:27:20 +0530

    Updated

--- Recursion/Kunal/GeeksForGeeksRecursion/ArmstrongNumber.java
+++ Recursion/Kunal/GeeksForGeeksRecursion/ArmstrongNumber.java
@@ -4,20 +4,19 @@ public class ArmstrongNumber {
 
     public static void main(String[] args) {
         int n = 1234;
-        double result =  isArmstongNUm(n,  (int)Math.log10(n) +1);
+        double result =  isArmstrongNUm(n,  (int)Math.log10(n) +1);
         System.out.println(result == (double)n);
 
         n = 153;
-        result =  isArmstongNUm(n,  (int)Math.log10(n) +1);
+        result =  isArmstrongNUm(n,  (int)Math.log10(n) +1);
         System.out.println(result == (double)n);
-
     }
 
-    private static double isArmstongNUm(int n, int digits) {
+    private static double isArmstrongNUm(int n, int digits) {
 
         if (n<=9) return Math.pow(n, digits);
 
-        return  Math.pow(n%10, digits) + (isArmstongNUm(n/10, digits));
+        return  Math.pow(n%10, digits) + (isArmstrongNUm(n/10, digits));
 
     }
 }
